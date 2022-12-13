b771bc6eff95f31d442fafe3b285cea022dbdac4
Carlos Vinicius <viniciusdev.26@gmail.com>
2022-12-13 09:41:39 -0300

    first lesson

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './database/Prisma.service';
+import { randomUUID } from 'node:crypto';
+import { CreateNotificationDTO } from './dtos/create-notification.dto';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async list() {
+    return await this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationDTO) {
+    const { content, category, recipientId } = body;
+    return await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
   }
 }
